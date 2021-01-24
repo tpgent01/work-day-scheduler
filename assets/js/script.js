f087cf39f0d1d71364737b953f389e79d857e6f9
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-01-24 12:55:25 -0600

    all hours added to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,7 +47,16 @@ $(document).ready(function(){
 
     // Get items from local storage
     $("#hour8 .description").val(localStorage.getItem("hour8"));
-
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+    $("#hour18 .description").val(localStorage.getItem("hour18"));
 
     timeTracker();
 })
