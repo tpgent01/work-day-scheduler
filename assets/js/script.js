66acd895607c8354ccff19cd5d3baa03d57ccf5f
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-01-24 12:07:32 -0600

    commented out js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
 // Display current date
 var todayDate = moment().format("dddd, MMM Do YYYY");
 $("#currentDay").html(todayDate);
+
+// Function to track time
+    // loop over time blocks
+        // check time for classes
+
+
+
+// Function to save user input
+    // save to local storage
+
+
+
+// Get items from local storage
